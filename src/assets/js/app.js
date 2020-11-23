513db6576113f1b4d139be5acca8c0d8011b6050
Igor Khromov <go11ga@rambler.ru>
2020-11-23 18:02:00 +0300

    fix: re-uploading

---
+++ src/assets/js/app.js
@@ -0,0 +1,11 @@
+//= ../../../node_modules/bootstrap/js/dist/util.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/slick-carousel/slick/slick.js
+//= components/nav.js
+
+
+$(function(){
+
+    //= components/slick.js
+
+});
---
+++ src/assets/js/components/nav.js
@@ -0,0 +1,33 @@
+/*
+var navs = document.querySelectorAll(".nav__link");
+console.log(1);
+
+for(var i = 0; i < navs.length; i++){
+    navs[i].addEventListener("click",changeNavColor);
+}
+
+
+function changeNavColor(event){
+    event.preventDefault();
+    for(var i = 0; i < navs.length; i++) {
+        navs[i].classList.remove("active");
+    }
+
+    this.classList.add("active");
+
+    console.log(this.href);
+
+   //document.location.replace(num);
+
+}
+*/
+
+
+
+
+
+
+
+
+
+
